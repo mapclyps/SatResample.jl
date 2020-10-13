945bef865fd1368288be3c1012ddfc75dfcf425c
Marcus Huntemann <marcus.huntemann@gmail.com>
2020-10-13 13:22:43 +0200

    added some tests for NSIDC grids

--- test/runtests.jl
+++ test/runtests.jl
@@ -32,3 +32,25 @@ using Test
     
 
 end
+
+@testset "grids" begin
+    #check NSIDC North and South grids for correct grid sizes
+    #see https://nsidc.org/data/polar-stereo/ps_grids.html
+    let genxy=SatResample.genxy
+        let NSIDCN = SatResample.NSIDCN
+            @test length.(genxy(NSIDCN(50000)))==(152,224)
+            @test length.(genxy(NSIDCN(25000)))==(304,448)
+            @test length.(genxy(NSIDCN(12500)))==(608,896)
+            @test length.(genxy(NSIDCN( 6250)))==(1216,1792)
+            @test length.(genxy(NSIDCN( 3125)))==(2432,3584)
+        end
+
+        let NSIDCS = SatResample.NSIDCS
+            @test length.(genxy(NSIDCS(50000)))==(158,166)
+            @test length.(genxy(NSIDCS(25000)))==(316,332)
+            @test length.(genxy(NSIDCS(12500)))==(632,664)
+            @test length.(genxy(NSIDCS( 6250)))==(1264,1328)
+            @test length.(genxy(NSIDCS( 3125)))==(2528,2656)
+        end
+    end
+end
